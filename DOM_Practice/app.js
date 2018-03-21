6defb5f2df8f965b60f8d30e64b2103fd434c908
Jafsari <justinafsari@yahoo.com>
2018-03-21 14:27:34 -0700

    adding more features

--- DOM_Practice/app.js
+++ DOM_Practice/app.js
@@ -1,4 +1,5 @@
 
+// delete books
 const list = document.querySelector('#book-list ul')
 list.addEventListener('click',function(e){
     if (e.target.className == 'delete'){
@@ -6,10 +7,56 @@ list.addEventListener('click',function(e){
         list.removeChild(li)
     }
 })
+// add books
+const addForm = document.forms['add-book']
+addForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    //grabbing the value from the form
+    const value = addForm.querySelector('input[type="text"]').value
 
+    //create elements
+    const li = document.createElement('li')
+    const bookName = document.createElement('span')
+    const deleteBtn = document.createElement('span')
 
+    // add Content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
 
+    //add Classes
+    deleteBtn.classList.add('delete')
+    bookName.classList.add('name')
+    
+    // append to the DOM
+    li.appendChild(bookName)
+    li.appendChild(deleteBtn)
+    //append the new li to the list
+    list.appendChild(li)
+})
 
+//hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change',function(e){
+    if (hideBox.checked){
+        list.style.display ='none'
+    } else {
+        list.style.display ='block'
+    }
+})
+
+const searchBar = document.forms['search-books'].querySelector("input");
+searchBar.addEventListener('keyup',function(e){
+    const term = e.target.value.toLowerCase()
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent
+        if (title.toLowerCase().indexOf(term) !== -1){
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
+})
 
 // var h2 = document.querySelector('#book-list h2')
 // h2.addEventListener("click",function(event){
