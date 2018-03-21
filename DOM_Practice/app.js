486fd4a96fe0d726b6a34746b93f204a961c7e49
Jafsari <justinafsari@yahoo.com>
2018-03-21 14:35:02 -0700

    adding dom content loaded

--- DOM_Practice/app.js
+++ DOM_Practice/app.js
@@ -1,6 +1,6 @@
-
+document.addEventListener('DOMContentLoaded',function(){
 // delete books
-const list = document.querySelector('#book-list ul')
+    const list = document.querySelector('#book-list ul')
 list.addEventListener('click',function(e){
     if (e.target.className == 'delete'){
         const li = e.target.parentElement;
@@ -116,3 +116,5 @@ bookList.previousElementSibling.querySelector('p').innerHTML += '<br> Charmander
 console.log(bookList.nextElementSibling.parentNode.children)
 console.log(bookList.previousElementSibling)
 console.log(bookList.nextElementSibling)
+})
+
