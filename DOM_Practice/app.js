8f602928696fae99ee6ebf06fb6cf641fe23ed61
Jafsari <justinafsari@yahoo.com>
2018-03-21 11:54:54 -0700

    adding node traversal through dom

---
+++ DOM_Practice/app.js
@@ -0,0 +1,45 @@
+// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf)
+// var books = document.querySelectorAll('#book-list li .name')
+// console.log(books)
+// books.forEach(function(book){
+// console.log(book)
+// });
+// books.forEach(function(book){
+//     book.textContent += '(book title)'
+// });
+
+// const bookList = document.querySelector('#book-list')
+// console.log(bookList.innerHTML)
+
+// bookList.innerHTML = '<h2> Books and more Books.... </h2>'
+// bookList.innerHTML += '<p> hello there friend </p>'
+
+// const banner = document.querySelector('#page-banner')
+// console.log('#page-banner node type is :', banner.nodeName)
+// console.log('#page-banner node type is :', banner.nodeType)
+// console.log('#page-banner node type is :', banner.hasChildNodes())
+
+// const clonedBanner = banner.clonedNode(true)
+
+
+//Traverse Upwards
+const bookList = document.querySelector('#book-list')
+// console.log('the parent node is:' ,bookList.parentNode.parentNode) // Pretty much the same as parent Element
+// console.log('the parent node is:' ,bookList.parentElement.parentElement)
+// //Traverse Downwards
+// console.log('the child node is', bookList.childNodes)
+// console.log(bookList.children)
+
+
+// console.log('the next sibling is:', bookList.nextSibling)
+// console.log('the next sibling is:', bookList.nextElementSibling)
+
+console.log('the previous sibling is:', bookList.previousSibling)
+console.log('the previous sibling is:', bookList.previousElementSibling)
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br> Charmander </br>'
+
+console.log(bookList.nextElementSibling.parentNode.children)
+console.log(bookList.previousElementSibling)
+console.log(bookList.nextElementSibling)
